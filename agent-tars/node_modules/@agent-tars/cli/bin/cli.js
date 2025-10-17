58332c626cf3cf951e8ba09b94752cdbda513098
zgit-ux <545800407@qq.com>
2025-10-17 16:40:42 +0800

    release of UI-TARS/agent-tars project
    This is the compile release of UI-TARS/agent-tars project.

---
+++ agent-tars/node_modules/@agent-tars/cli/bin/cli.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env -S node --no-warnings
+/*
+ * Copyright (c) 2025 Bytedance, Inc. and its affiliates.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+const { version } = require('../package.json');
+require('../dist').bootstrapCli({ version, binName: 'agent-tars' });
